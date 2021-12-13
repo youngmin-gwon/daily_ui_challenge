45bb44e1cb5d237408b0b66751fd4d699827c7e9
enkanto <deukman@users.noreply.github.com>
2021-12-13 23:14:30 +0900

    daily ui 1 working

--- lib/2021/12/13/20211213.dart
+++ lib/2021/12/13/20211213.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Screen20211213 extends StatelessWidget {
+class Screen20211213 extends StatefulWidget {
   const Screen20211213({Key? key}) : super(key: key);
+
+  @override
+  State<Screen20211213> createState() => _Screen20211213State();
+}
+
+class _Screen20211213State extends State<Screen20211213>
+    with SingleTickerProviderStateMixin {
+  bool _isClicked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +26,48 @@ class Screen20211213 extends StatelessWidget {
         ),
       ),
       body: Stack(
+        clipBehavior: Clip.hardEdge,
         children: [
           Positioned(
-            left: MediaQuery.of(context).size.width / 2,
+            left: 0,
+            right: 0,
             bottom: MediaQuery.of(context).size.height / 4,
-            child: const CustomPaint(
-              painter: Circle(
-                strokeWidth: 3,
-                strokeColor: Colors.yellow,
-                radius: 36,
-                center: Offset(0, 0),
-                isFilled: false,
-              ),
-            ),
-          ),
-          Positioned(
-            left: MediaQuery.of(context).size.width / 2,
-            bottom: MediaQuery.of(context).size.height / 4,
-            child: const CustomPaint(
-              painter: Circle(
-                strokeWidth: 2,
-                strokeColor: Colors.white,
-                radius: 24,
-                center: Offset(0, 0),
-                isFilled: false,
+            child: FractionalTranslation(
+              translation: const Offset(0.5, 0.5),
+              child: Listener(
+                onPointerDown: (event) {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: Stack(
+                  children: [
+                    CustomPaint(
+                      painter: Circle(
+                        strokeWidth: 2,
+                        strokeColor: Colors.white,
+                        radius: 24,
+                        isFilled: _isClicked,
+                      ),
+                      child: const SizedBox(
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    const CustomPaint(
+                      painter: Circle(
+                        strokeWidth: 3,
+                        strokeColor: Colors.yellow,
+                        radius: 32,
+                        isFilled: false,
+                      ),
+                      child: SizedBox(
+                        width: 32,
+                        height: 32,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -62,7 +88,7 @@ class Circle extends CustomPainter {
       {required this.strokeWidth,
       required this.strokeColor,
       required this.radius,
-      required this.center,
+      this.center = const Offset(0.0, 0.0),
       required this.isFilled});
 
   @override
