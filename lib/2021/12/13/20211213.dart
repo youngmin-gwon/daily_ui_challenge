4f1980b5d655847b110ee07caca1f16d69a20f24
youngmin-gwon <youngmin.gwon@gmail.com>
2021-12-14 19:03:30 +0900

    daily ui 1: basic ui work

--- lib/2021/12/13/20211213.dart
+++ lib/2021/12/13/20211213.dart
@@ -1,4 +1,6 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Screen20211213 extends StatefulWidget {
   const Screen20211213({Key? key}) : super(key: key);
@@ -9,7 +11,18 @@ class Screen20211213 extends StatefulWidget {
 
 class _Screen20211213State extends State<Screen20211213>
     with SingleTickerProviderStateMixin {
-  bool _isClicked = false;
+  bool _isOn = false;
+
+  final _onLightSvg = "assets/svg/light_on.svg";
+  final _offLightSvg = "assets/svg/light_off.svg";
+
+  late String _lightSvg;
+
+  @override
+  void initState() {
+    super.initState();
+    _lightSvg = _offLightSvg;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,43 +41,52 @@ class _Screen20211213State extends State<Screen20211213>
       body: Stack(
         clipBehavior: Clip.hardEdge,
         children: [
+          const SizedBox.expand(),
+          Center(
+            child: Transform.rotate(
+              angle: math.pi,
+              child: Transform.translate(
+                offset: const Offset(0, 90),
+                child: SvgPicture.asset(
+                  _lightSvg,
+                  width: 200,
+                  height: 200,
+                ),
+              ),
+            ),
+          ),
           Positioned(
-            left: 0,
-            right: 0,
+            left: MediaQuery.of(context).size.width / 2,
             bottom: MediaQuery.of(context).size.height / 4,
             child: FractionalTranslation(
-              translation: const Offset(0.5, 0.5),
-              child: Listener(
-                onPointerDown: (event) {
+              translation: const Offset(-0.5, 0.5),
+              child: GestureDetector(
+                onTapDown: (details) {
                   setState(() {
-                    _isClicked = !_isClicked;
+                    _isOn = true;
+                    _lightSvg = _onLightSvg;
+                  });
+                },
+                onTapUp: (details) {
+                  setState(() {
+                    _isOn = false;
+                    _lightSvg = _offLightSvg;
                   });
                 },
                 child: Stack(
+                  alignment: Alignment.center,
                   children: [
-                    CustomPaint(
-                      painter: Circle(
-                        strokeWidth: 2,
-                        strokeColor: Colors.white,
-                        radius: 24,
-                        isFilled: _isClicked,
-                      ),
-                      child: const SizedBox(
-                        width: 24,
-                        height: 24,
-                      ),
+                    CircleWidget(
+                      radius: 20,
+                      width: 2,
+                      color: Colors.white,
+                      isOn: _isOn,
                     ),
-                    const CustomPaint(
-                      painter: Circle(
-                        strokeWidth: 3,
-                        strokeColor: Colors.yellow,
-                        radius: 32,
-                        isFilled: false,
-                      ),
-                      child: SizedBox(
-                        width: 32,
-                        height: 32,
-                      ),
+                    CircleWidget(
+                      radius: 30,
+                      width: 3,
+                      color: Colors.yellow,
+                      isOn: false,
                     ),
                   ],
                 ),
@@ -72,6 +94,39 @@ class _Screen20211213State extends State<Screen20211213>
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CircleWidget extends StatelessWidget {
+  const CircleWidget({
+    Key? key,
+    required this.radius,
+    required this.width,
+    required this.color,
+    required this.isOn,
+  }) : super(key: key);
+
+  final double radius;
+  final double width;
+  final Color color;
+  final bool isOn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: isOn ? color : null,
+        border: Border.all(
+          color: color,
+          width: width,
+        ),
+        shape: BoxShape.circle,
+      ),
+      child: SizedBox(
+        height: radius * 2,
+        width: radius * 2,
       ),
     );
   }
@@ -88,7 +143,7 @@ class Circle extends CustomPainter {
       {required this.strokeWidth,
       required this.strokeColor,
       required this.radius,
-      this.center = const Offset(0.0, 0.0),
+      this.center = const Offset(0.5, -0.5),
       required this.isFilled});
 
   @override
@@ -102,7 +157,7 @@ class Circle extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+  bool shouldRepaint(covariant Circle oldDelegate) {
+    return oldDelegate.isFilled != isFilled;
   }
 }
