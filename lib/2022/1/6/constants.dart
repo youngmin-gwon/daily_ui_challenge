a59c1929f1ddabac02e3c5b4a49ae218765fac75
youngmin-gwon <youngmin.gwon@gmail.com>
2022-01-06 18:06:22 +0900

    Edit: 추가 작업 중
    무엇을: 미완성 작업 중

---
+++ lib/2022/1/6/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// colors
+const Color kBlue = Color(0xFF306EFF);
+const Color kLightBlue = Color(0xFF4985FD);
+const Color kDarkBlue = Color(0xFF1046B3);
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kGrey = Color(0xFFF3F5F7);
+
+// padding
+const double kPaddingS = 8.0;
+const double kPaddingM = 16.0;
+const double kPaddingL = 32.0;
+
+// spacing
+const double kSpaceS = 8.0;
+const double kSpaceM = 16.0;
+
+// Assets
+const String kGoogleLogoPath = 'assets/images/google_logo.png';
