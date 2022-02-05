0fdfe5739d8d68002720fc43008ae377d2d6a119
beenawhile <beenawhile@users.noreply.github.com>
2022-02-05 23:59:30 +0900

    paint

--- lib/2022/2/february.dart
+++ lib/2022/2/february.dart
@@ -1,4 +1,5 @@
 import 'package:daily_ui/2022/2/4/instagram_animation.dart';
+import 'package:daily_ui/2022/2/5/path_selection_screen.dart';
 import 'package:flutter/material.dart';
 
 class February2022Screen extends StatelessWidget {
@@ -22,6 +23,17 @@ class February2022Screen extends StatelessWidget {
                   ));
                 },
                 child: const Text("4"),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) {
+                      return const PathSelectionScreen();
+                    },
+                  ));
+                },
+                child: const Text("5"),
               ),
               const SizedBox(height: 12),
             ],
