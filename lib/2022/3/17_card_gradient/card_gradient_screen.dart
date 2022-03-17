04f9614f584ca9954dfca4c8f5bf9b4cf54f5218
youngmin-gwon <youngmin.gwon@gmail.com>
2022-03-17 23:18:26 +0900

    Feat(animation): edit 2022-03-17 daily animation
    finish card animation until gradient effect

--- lib/2022/3/17_card_gradient/card_gradient_screen.dart
+++ lib/2022/3/17_card_gradient/card_gradient_screen.dart
@@ -13,23 +13,31 @@ class CardGradientScreen extends StatefulWidget {
 class _CardGradientScreenState extends State<CardGradientScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+
+  /// angleTween : angles to show dynamic gradient effect
+  ///  range = [-pi/10, pi/10]
+  static final _angleTween =
+      Tween<double>(begin: math.pi / 8, end: -math.pi / 8).chain(
+    CurveTween(
+      curve: Curves.easeIn,
+    ),
+  );
   late Size size;
 
-  /// y : angles to show dynamic gradient effect
-  ///  range = [-pi/10, pi/10]
-  double y = 0;
+  bool isLeftSide(double x) {
+    return x < size.width / 2;
+  }
+
   void _onLongPressDown(LongPressDownDetails details) {
-    print('global:${details.globalPosition.dx}');
-    print('local:${size.width}');
-    if (details.globalPosition.dx < size.width / 2) {
-      setState(() {
-        y = math.pi / 10;
-      });
-    } else if (details.globalPosition.dx > size.width / 2) {
-      setState(() {
-        y = -math.pi / 10;
-      });
+    if (isLeftSide(details.globalPosition.dx)) {
+      _animationController.animateBack(0);
+    } else {
+      _animationController.animateTo(1);
     }
+  }
+
+  void _onPressEnd() {
+    _animationController.animateTo(0.5);
   }
 
   @override
@@ -37,8 +45,9 @@ class _CardGradientScreenState extends State<CardGradientScreen>
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 800),
+      duration: const Duration(milliseconds: 500),
     );
+    _animationController.value = 0.5;
   }
 
   @override
@@ -50,6 +59,8 @@ class _CardGradientScreenState extends State<CardGradientScreen>
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
+    final usimWidth = size.width / 12;
+    final usimHeight = usimWidth * 1.5;
     return Scaffold(
       extendBodyBehindAppBar: true,
       backgroundColor: Colors.grey[850],
@@ -59,27 +70,113 @@ class _CardGradientScreenState extends State<CardGradientScreen>
       ),
       body: Center(
         child: SizedBox(
-          width: size.width / 2.5,
+          width: size.width / 2,
           child: AspectRatio(
-            aspectRatio: 3 / 5,
-            child: Transform(
-              transform: Matrix4.identity()
-                ..setEntry(3, 2, 0.001)
-                ..rotateY(y)
-                ..rotateZ(-math.pi / 12),
-              alignment: FractionalOffset.center,
-              child: GestureDetector(
-                onLongPressDown: _onLongPressDown,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.red,
+            aspectRatio: 19 / 30,
+            child: AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Transform(
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateX(-math.pi / 12)
+                    ..rotateY(_angleTween.animate(_animationController).value)
+                    ..rotateZ(-math.pi / 12),
+                  alignment: FractionalOffset.center,
+                  child: GestureDetector(
+                    onLongPressDown: _onLongPressDown,
+                    onLongPressEnd: (_) {
+                      _onPressEnd();
+                    },
+                    onTapUp: (_) {
+                      _onPressEnd();
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.red,
+                        gradient: RadialGradient(
+                          colors: [
+                            Colors.white,
+                            Colors.red,
+                          ],
+                          center: Alignment(
+                            0,
+                            0,
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Usim(width: usimWidth, height: usimHeight),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Hello',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    ?.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                              ),
+                              const Icon(
+                                Icons.workspaces_rounded,
+                                color: Colors.white,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Usim extends StatelessWidget {
+  const Usim({
+    Key? key,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: const Color(0xFFE0A91F),
+      ),
+      width: width,
+      height: height,
+      child: Row(
+        children: [
+          const Expanded(child: SizedBox()),
+          Container(
+            color: Colors.black,
+            width: 1,
+          ),
+          const Expanded(child: SizedBox()),
+          Container(
+            color: Colors.black,
+            width: 1,
+          ),
+          const Expanded(child: SizedBox()),
+        ],
       ),
     );
   }
