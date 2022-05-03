ca16f5d830d1170de69dc596b5c33069a5dcee97
youngmin-gwon <youngmin.gwon@gmail.com>
2022-05-03 22:10:49 +0900

    Feat(animation): add 2022-05-04 animation
    add particle moving animation

--- lib/2022/5/may.dart
+++ lib/2022/5/may.dart
@@ -1,5 +1,6 @@
 import 'package:daily_ui/2022/5/1_neon_light/neon_light_screen.dart';
 import 'package:daily_ui/2022/5/2_moving_gradation/moving_gradation_screen.dart';
+import 'package:daily_ui/2022/5/4_particle_sweep/particle_sweep_screen.dart';
 import 'package:flutter/material.dart';
 
 class MayScreen extends StatelessWidget {
@@ -35,6 +36,17 @@ class MayScreen extends StatelessWidget {
                   );
                 },
                 child: const Text("2. Moving Gradation Animation"),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const ParticleSweepScreen(),
+                    ),
+                  );
+                },
+                child: const Text("4. Particle Sweep Animation"),
               ),
             ],
           ),
