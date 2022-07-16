1a3e8b36f51228bb551e9baa66bc91355d8f8a35
youngmin-gwon <youngmin.gwon@gmail.com>
2022-07-16 23:54:03 +0900

    Feat(ui): wave basic

--- lib/2022/7/12_wave_practice/wave_slider.dart
+++ lib/2022/7/12_wave_practice/wave_slider.dart
@@ -126,118 +126,11 @@ class WavePainter extends CustomPainter {
         fillPaint);
   }
 
-  void _paintWaveLine(Canvas canvas, Size size) {
-    WaveCurveDefinitions waveCurve = _calculateWaveLineDefinition();
-
-    final path = Path();
-    path.moveTo(0, size.height);
-    path.lineTo(waveCurve.startOfBezier, size.height);
-    path.cubicTo(
-      waveCurve.leftControlPoint1,
-      size.height,
-      waveCurve.leftControlPoint2,
-      waveCurve.controlHeight,
-      waveCurve.centerPoint,
-      waveCurve.controlHeight,
-    );
-    path.cubicTo(
-      waveCurve.rightControlPoint1,
-      waveCurve.controlHeight,
-      waveCurve.rightControlPoint2,
-      size.height,
-      waveCurve.endOfBezier,
-      size.height,
-    );
-    path.lineTo(size.width, size.height);
-
-    canvas.drawPath(path, linePaint);
-  }
-
-  WaveCurveDefinitions _calculateWaveLineDefinition() {
-    double bendWidth = 40;
-    double bezierWidth = 40;
-
-    double startOfBend = sliderPosition - bendWidth / 2;
-    double startOfBezier = startOfBend - bezierWidth;
-    double endOfBend = sliderPosition + bendWidth / 2;
-    double endOfBezier = endOfBend + bezierWidth;
-
-    double controlHeight = 0.0;
-    double centerPoint = sliderPosition;
-
-    double leftControlPoint1 = startOfBend;
-    double leftControlPoint2 = startOfBend;
-    double rightControlPoint1 = endOfBend;
-    double rightControlPoint2 = endOfBend;
-
-    // how bendable the curve would be
-    double bendability = 25.0;
-    double maxSlideDifference = 20.0;
-
-    double slideDifference = (sliderPosition - _previousSliderPosition).abs();
-
-    if (slideDifference > maxSlideDifference) {
-      slideDifference = maxSlideDifference;
-    }
-
-    double bend =
-        lerpDouble(0.0, bendability, slideDifference / maxSlideDifference)!;
-
-    bool moveLeft = sliderPosition < _previousSliderPosition;
-
-    if (moveLeft) {
-      leftControlPoint1 = leftControlPoint1 - bend;
-      leftControlPoint2 = leftControlPoint2 + bend;
-      rightControlPoint1 = rightControlPoint1 + bend;
-      rightControlPoint2 = rightControlPoint2 - bend;
-      centerPoint = centerPoint + bend;
-    } else {
-      leftControlPoint1 = leftControlPoint1 + bend;
-      leftControlPoint2 = leftControlPoint2 - bend;
-      rightControlPoint1 = rightControlPoint1 - bend;
-      rightControlPoint2 = rightControlPoint2 + bend;
-      centerPoint = centerPoint - bend;
-    }
-
-    WaveCurveDefinitions waveCurve = WaveCurveDefinitions(
-      controlHeight: controlHeight,
-      startOfBezier: startOfBezier,
-      endOfBezier: endOfBezier,
-      leftControlPoint1: leftControlPoint1,
-      leftControlPoint2: leftControlPoint2,
-      rightControlPoint1: rightControlPoint1,
-      rightControlPoint2: rightControlPoint2,
-      centerPoint: centerPoint,
-    );
-
-    return waveCurve;
-  }
+  void _paintWaveLine(Canvas canvas, Size size) {}
 
   @override
   bool shouldRepaint(covariant WavePainter oldDelegate) {
     _previousSliderPosition = oldDelegate.sliderPosition;
     return true;
   }
-}
-
-class WaveCurveDefinitions {
-  final double startOfBezier;
-  final double endOfBezier;
-  final double leftControlPoint1;
-  final double leftControlPoint2;
-  final double rightControlPoint1;
-  final double rightControlPoint2;
-  final double controlHeight;
-  final double centerPoint;
-
-  WaveCurveDefinitions({
-    required this.startOfBezier,
-    required this.endOfBezier,
-    required this.leftControlPoint1,
-    required this.leftControlPoint2,
-    required this.rightControlPoint1,
-    required this.rightControlPoint2,
-    required this.controlHeight,
-    required this.centerPoint,
-  });
 }
